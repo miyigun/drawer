f9d855ecf851363582ca05f4aee7b7e146f66cf4
miyigun2017 <miyigun2017@gmail.com>
2022-02-25 18:06:01 +0300

    2

--- lib/screens/screen_log_out.dart
+++ lib/screens/screen_log_out.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class LogOut extends StatelessWidget{
+  const LogOut({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Log Out"),
+          title: const Text("Log Out"),
       ),
     );
   }
--- lib/screens/screen_main_screen.dart
+++ lib/screens/screen_main_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget{
+  const MainScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Main Screen")
+        title: const Text("Main Screen")
       ),
     );
   }
